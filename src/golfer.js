e95a1e735d5e99dd0ad66a2c00d441ab2ecccab9
Ashur Cabrera <hello@ashur.cab>
2019-07-27 05:56:02 +0000

    Add ball, clubs, and ability to swing

--- src/golfer.js
+++ src/golfer.js
@@ -1,3 +1,5 @@
+const Ball = require( './ball' );
+
 class Golfer
 {
 	constructor( { firstName, middleInitial, lastName } )
@@ -5,12 +7,28 @@ class Golfer
 		this.firstName = firstName;
 		this.middleInitial = `${middleInitial}.`;
 		this.lastName = lastName;
+
+		this.ball = new Ball();
+		this.clubs = [];
+	}
+
+	club( club )
+	{
+		this.clubs.push( club );
 	}
 
 	get fullName()
 	{
 		return [this.firstName, this.middleInitial, this.lastName]
 			.join( ' ' );
+	}
+
+	swing( clubIndex, expected )
+	{
+		let club = this.clubs[clubIndex];
+		let actual = expected * club.power;
+
+		this.ball.distance += actual;
 	}
 }
 
