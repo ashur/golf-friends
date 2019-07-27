a6b241ec19cae3dd45ce579f54458baa57f66277
Ashur Cabrera <hello@ashur.cab>
2019-07-27 05:54:42 +0000

    Convert getters to properties

--- src/club.js
+++ src/club.js
@@ -1,5 +1,8 @@
 const crypto = require( 'crypto' );
 
+let maxHex = 'fff';
+let maxValue = parseInt( maxHex, 16 )
+
 class Club
 {
 	constructor( name )
@@ -9,32 +12,22 @@ class Club
 			.update( name )
 			.digest( 'hex' );
 
-		this.maxHex = 'fff';
-		this.maxValue = parseInt( this.maxHex, 16 )
+		this.accuracy = this.valueOf( this.subhash( 0 ) );
+		this.power = this.valueOf( this.subhash( 1 ) );
 	}
 
 	subhash( index )
 	{
-		let start = index * this.maxHex.length;
-		let end = index * this.maxHex.length + this.maxHex.length
+		let start = index * maxHex.length;
+		let end = index * maxHex.length + maxHex.length
 
 		return this.hash.substring( start, end );
-	}
-
-	get accuracy()
-	{
-		return this.valueOf( this.subhash( 0 ) );
-	}
-
-	get power()
-	{
-		return this.valueOf( this.subhash( 1 ) );
 	}
 
 	valueOf( subhash )
 	{
 		let raw = parseInt( subhash, 16 );
-		return (raw / this.maxValue).toFixed( 3 );
+		return (raw / maxValue).toFixed( 3 );
 	}
 }
 
